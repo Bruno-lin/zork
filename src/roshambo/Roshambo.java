7d7f4b7267a51eda9ec5db70ac6a351b08caa334
brunozzz <16675056030>
2021-01-21 09:01:24 +0800

    the bug has been modified

--- src/roshambo/Roshambo.java
+++ src/roshambo/Roshambo.java
@@ -23,26 +23,26 @@ public class Roshambo extends ConsoleProgram {
             int index = randomGenerator.nextInt(0, 2);  // 产生一个0-2之间的随机数
             String computerShape = SHAPES[index];                  // 根据此随机数从数组中选择一个手势
 
-            if (yourShape == "石头") {
-                if (computerShape == "剪刀") {
+            if (yourShape.equals("石头")) {
+                if (computerShape.equals("剪刀")) {
                     printWinMessage(yourShape, computerShape);
-                } else if (computerShape == "布") {
+                } else if (computerShape.equals("布")) {
                     printLoseMessage(yourShape, computerShape);
                 } else {
                     printTieMessage(yourShape);
                 }
-            } else if (yourShape == "剪刀") {
-                if (computerShape == "布") {
+            } else if (yourShape.equals("剪刀")) {
+                if (computerShape.equals("布")) {
                     printWinMessage(yourShape, computerShape);
-                } else if (computerShape == "石头") {
+                } else if (computerShape.equals("石头")) {
                     printLoseMessage(yourShape, computerShape);
                 } else {
                     printTieMessage(yourShape);
                 }
-            } else if (yourShape == "布") {
-                if (computerShape == "石头") {
+            } else if (yourShape.equals("布")) {
+                if (computerShape.equals("石头")) {
                     printWinMessage(yourShape, computerShape);
-                } else if (computerShape == "石头") {
+                } else if (computerShape.equals("剪刀")) {
                     printLoseMessage(yourShape, computerShape);
                 } else {
                     printTieMessage(yourShape);
@@ -56,8 +56,9 @@ public class Roshambo extends ConsoleProgram {
 
     /**
      * 打印获胜的信息
-     * @param yourShape         你出的手势
-     * @param computerShape     电脑出的手势
+     *
+     * @param yourShape     你出的手势
+     * @param computerShape 电脑出的手势
      */
     public void printWinMessage(String yourShape, String computerShape) {
         println("你赢了！" + "你出的是『" + yourShape + "』，电脑出的是『" + computerShape + "』。");
@@ -65,8 +66,9 @@ public class Roshambo extends ConsoleProgram {
 
     /**
      * 打印输掉的信息
-     * @param yourShape         你出的手势
-     * @param computerShape     电脑出的手势
+     *
+     * @param yourShape     你出的手势
+     * @param computerShape 电脑出的手势
      */
     public void printLoseMessage(String yourShape, String computerShape) {
         println("你输了！" + "你出的是『" + yourShape + "』，电脑出的是『" + computerShape + "』。");
@@ -74,7 +76,8 @@ public class Roshambo extends ConsoleProgram {
 
     /**
      * 打印平局信息
-     * @param shape             你跟电脑的手势，应该是一样的，所以平局
+     *
+     * @param shape 你跟电脑的手势，应该是一样的，所以平局
      */
     public void printTieMessage(String shape) {
         println("平局。你和电脑出的都是『" + shape + "』。");
